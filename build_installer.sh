33892dbacf771fe230d72794d921c558a356a93b
mrkksparrow <mkavink2000@gmail.com>
2025-03-20 13:01:46 +0530

    push sample install file

---
+++ build_installer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+KUBERNETES_PROJECT=/home/kavin-11438/IdeaProjects/Agent/PresentWorkItem/kubernetes/source/python3.3/src/com/manageengine/monagent/kubernetes
+PYTHON_AGENT_FRAMEWORK=/home/kavin-11438/IdeaProjects/Agent/PresentWorkItem/python_agent_framework/source/python3.3/src/com/manageengine/monagent/
+
+SCRIPT_PATH=$(realpath "$0")
+PROJECT_DIR=$(dirname "$SCRIPT_PATH")
+INSTALL_FILE_LENGTH=$(head -n 100 "$PROJECT_DIR"/Site24x7MonitoringAgent.install | grep "INSTALL_FILE_LENGTH" | cut -d '=' -f2)
+INSTALL_FILE_LENGTH=$((INSTALL_FILE_LENGTH - 1))
+
+head -n $INSTALL_FILE_LENGTH "$PROJECT_DIR"/Site24x7MonitoringAgent.install > "$PROJECT_DIR"/Site24x7MonitoringAgent.install.1
+
+TEMP_PROJECT_DIR=$PROJECT_DIR/../temp/devops/monagent/lib/devops/source/python3.3/src/com/manageengine/monagent/
+cp -rf $PYTHON_AGENT_FRAMEWORK $TEMP_PROJECT_DIR
+cp -rf $KUBERNETES_PROJECT $TEMP_PROJECT_DIR
+
+# shellcheck disable=SC2164
+cd $PROJECT_DIR/../temp
+tar -czvf devops.tar.gz devops/
+
+cat devops.tar.gz >> "$PROJECT_DIR"/Site24x7MonitoringAgent.install.1
+mv "$PROJECT_DIR"/Site24x7MonitoringAgent.install.1 "$PROJECT_DIR"/Site24x7MonitoringAgent.install
+
+cd $PROJECT_DIR
+git add Site24x7MonitoringAgent.install
+git commit -m “added”
+
+echo "=========> Pushing Changes to GitHub <========="
+git push
